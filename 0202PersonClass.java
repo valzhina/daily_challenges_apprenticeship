24a731dec3f350f5e7e43b3971a356ba69f37f76
Valzhina <valzhinapiskun@gmail.com>
2022-09-29 20:08:26 -0700

    introduce method

--- 0202PersonClass.java
+++ 0202PersonClass.java
@@ -1,7 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Person person1 = new Person("Bob", 28);
+        Student student1 = new Student("Alphonse", 30);
+        Teacher teacher1 = new Teacher("Lily", 22);
+        Teacher teacher2 = new Teacher("Jess", 20);
+
+        person1.introduce();
+        student1.introduce();
+        teacher1.introduce("Alphonse");
+        teacher2.introduce();
     }
 }
 class Person {
@@ -26,6 +34,8 @@ class Person {
 //        this.age = 99;
     }
 
+    //    ======================================
+//    CONSTRUCTOR
     public String getName() { return name;}
     public int getAge() { return age;}
 
@@ -40,9 +50,17 @@ class Person {
         }
     }
 
+    public void introduce(){
+        System.out.println("Hi, my name is " + getName() + ".");
+    }
+
     @Override
     public String toString() {
-        return name + ", " + age;
+        //return name + ", " + age;
+        return "{ name = " + this.name + ", \n" +
+                "age = " + this.age + "\n" +
+                "}\n";
+
     }
 
     public boolean equals(Person otherPerson) {
@@ -54,3 +72,127 @@ class Person {
     }
 }
 
+class Student extends Person {
+    private char grade;
+    //private char grade = 'B'; //default grade
+
+    public Student() {
+        //super("Student", 12);
+        //this.grade = 'B';
+        //setGrade ('B');
+        this("Student", 12,'B');
+    }
+
+    public Student(String name, int age) {
+        super(name, age);
+        this.grade = grade;
+        //setGrade(grade);
+    }
+
+    public Student(String name, int age, char grade) {
+        super(name, age);
+        this.grade = grade;
+        //setGrade(grade);
+    }
+
+    public char getGrade(){
+        return grade;
+    }
+
+    public void setGrade(char grade) {
+/*
+        if ((int)grade >= 65 && (int)grade <= 70 && (int)grade != 69){
+            this.grade = grade;
+        }
+        else if ((int)grade >= 97 && (int)grade <= 102 && (int)grade != 101){
+            Character.toUpper(grade);
+            this.grade = grade;
+        }
+        else{
+            System.out.println("Invalid char for grade: one of four allowed chars");
+        }
+*/
+
+/*
+        switch(grade){
+            case 'A':
+            case 'B':
+            case 'C':
+            case 'D':
+            case 'F':
+                Character.toLowerCase(grade);
+            case 'a':
+            case 'b':
+            case 'c':
+            case 'd':
+            case 'f':
+                this.grade = grade;
+                break;
+            default:
+                System.out.println("Invalid char for grade: one of four allowed chars");
+        }
+*/
+
+        Character.toUpperCase(grade);
+        if (grade == 'A' || grade == 'B' || grade == 'C' || grade == 'D') {
+            this.grade = grade;
+        }
+        else{
+            System.out.println("Invalid char for grade: one of four allowed chars");
+        }
+/*
+        Character.toUpperCase(grade);
+        char[] validGrades = {'A', 'B', 'C', 'D'};
+
+        for(char v: validGrades){
+            if(grade == v){
+
+            }
+        }
+        */
+
+    }
+}
+
+
+class Teacher extends Person {
+    private int salary;
+
+    public Teacher() {
+        //super("Teacher", 120000);
+        //this.salary = 120000;
+        //setSalary (120000);
+        this("Teacher", 47,120000);
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int salary) {
+        super(name, age);
+        this.salary = salary;
+    }
+
+
+    public int getSalary(){
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        if (salary >= 150000) {
+            this.salary = salary;
+        }
+        else{
+            System.out.println("Invalid int for salary: should be higher or equal 150K");
+        }
+    }
+    public void introduce(){
+        System.out.println("Hello, I'm Professor " + getName() + ".");
+        //System.out.printf("Hi, my name is %s and I am %d2.2 years old." this.getName(), this.getAge());
+        //System.out.printf("Hi, my name is %s and I am %d years old." getName(), getAge());
+    }
+    public void introduce(String otherName){
+        System.out.println("Nice to meet you, " + otherName + "! I'm Professor " + getName() + ".");
+    }
+}

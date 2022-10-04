95f106ba8b43e83f1c0bcadae8c70ff271c56922
Valzhina <valzhinapiskun@gmail.com>
2022-10-03 22:29:56 -0700

    updated

--- 322CoinChangeChallenge.java
+++ 322CoinChangeChallenge.java
@@ -12,23 +12,6 @@ public class Challenge322CoinChange {
         // coins = {1,5,10,25}
         // amount = 13
 
-    public static int fib(int numberToFind){
-        int[] outputs = new int[amount+1];
-        Arrays.fill(outputs, -1);
-
-        if(outputs[numberToFind] != -1){
-            return  outputs[numberToFind];
-        }
-        else{
-            outputs[numberToFind] = fib(numberToFind-1)
-        }
-        return fib(numberToFind-1) + fib(numberToFind -1);
-    }
-
-
-
-//        {0,1,2,3,4,5}
-
         Arrays.fill(outputs, 1, outputs.length, amount + 1);
         //[0, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14]
         //[0, 6, 6, 6, 6, 6]
